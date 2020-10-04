3b5a6b4ad4bc5bbe3d94c6c416eb4b53e7b1f7da
Chris Roschi <chrisroschi53@gmail.com>
2020-10-04 14:23:37 +1030

    Updates

---
+++ precache-manifest.ae287cc314a72e9d110590dc1581ac1e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dee1f440d16c72cf5b9bdc0495d73558",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19d5e209fb800c0735ee",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3418fe1bdebd947547bc",
+    "url": "/static/css/main.4bd00dc4.chunk.css"
+  },
+  {
+    "revision": "19d5e209fb800c0735ee",
+    "url": "/static/js/2.0c5edc8d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0c5edc8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3418fe1bdebd947547bc",
+    "url": "/static/js/main.85982178.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "7779f7f2dd456d35ee0bb08eba6b3aac",
+    "url": "/static/media/logo.7779f7f2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Portfolio/precache-manifest.0347eab2dddf3c46d8682bc67a4df768.js"
+  "/precache-manifest.ae287cc314a72e9d110590dc1581ac1e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
